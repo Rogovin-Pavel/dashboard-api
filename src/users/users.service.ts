a26ec2b29f8d41769b9a2d3a0aea7d47ef8b7c15
p.rogovin <p.rogovin@devinotele.com>
2023-01-09 23:10:27 +0400

    [V84] dotnet, config and salt are added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,19 @@
+import { inject, injectable } from 'inversify';
+
 import { User } from './user.entity';
+import { TYPES } from '../types';
 import { UserLoginDto } from './dto/user-login.dto';
+import { IConfigService } from '../config/config.service.interface';
 import { IUsersService } from './users.service.interface';
 import { UserRegisterDto } from './dto/user-register.dto';
 
-import { injectable } from 'inversify';
-
 @injectable()
 export class UsersService implements IUsersService {
+  constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
   async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
     const newUser = new User(email, name);
-    await newUser.setPassword(password);
+    const salt = Number(this.configService.get('SALT'));
+    await newUser.setPassword(password, salt);
     // validate user in db. user ? null : new user
     return newUser || null;
   }
